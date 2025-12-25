82cd8053574350323e1587f5dd811c930d55e9f0
syawaloktasyahputra <syawaloktasyahputra@gmail.com>
2025-12-26 01:03:17 +0700

    update config

--- example.config.js
+++ example.config.js
@@ -5,6 +5,26 @@ export default {
   CHANNEL_URL: "https://t.me/CHANNEL",
   PANEL_URL: "YOUR_PANEL_URL",
   APPLICATION_API_KEY: "YOUR_APLICATION_API_KEY",
+  CLIENT_API_KEY: "YOUR_CLIRNT_API_KET",
   DEFAULT_LOCATION_ID: 1,
-  DEFAULT_EGG_ID: 17
+  DEFAULT_EGG_ID: 17,
+  DEPOSIT: {
+    MIN: 5000,
+    TIMEOUT: 5 * 60 * 1000, // 5 menit
+
+    TARGET: {
+      DANA: {
+        number: "085722713244",
+        name: "A.N Muhammad Ikhwanul Fata"
+      },
+      GOPAY: {
+        number: "085722713244",
+        name: "A.N Muhammad Ikhwanul Fata"
+      },
+      QRIS: {
+        link: "https://your-qris-link.com",
+        name: "VOLTRAPEDIA OFFICIAL"
+      }
+    }
+  }
 };
